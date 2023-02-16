2da8958dd8eb60bb415739bce1e25d7c144a56ed
samarth-joveo <104884127+samarth-joveo@users.noreply.github.com>
2023-02-16 08:04:33 +0530

    Add files via upload

---
+++ new_file.js
@@ -0,0 +1,66 @@
+looker.plugins.visualizations.add({
+  create: function(element, config) {
+  },
+  updateAsync: function(data, element, config, queryResponse, details, done) {
+    clicks_total = 0
+
+data.forEach((d)=>{
+  clicks_total += d["Spend"]
+})
+element.innerHTML = `
+     <style>
+.borderStyle{
+border: 1px solid #EAECF0;
+border-radius: 12px;
+padding-top: 26px;
+padding-left: 56px;
+
+}
+.componentOne {
+font-family: 'Inter';
+font-style: normal;
+font-weight: 500;
+font-size: 14px;
+}
+.componentTwo {
+font-family: 'SF Pro Display';
+font-style: normal;
+font-weight: 500;
+font-size: 32px;
+padding-top: 5px;
+padding-bottom: 5px;
+}
+.componentThree {
+font-family: 'SF Pro Text';
+font-style: normal;
+font-weight: 500;
+font-size: 14px;
+padding-bottom: 60px;
+}
+</style>
+</head>
+<body>
+
+
+
+<div class="borderStyle">
+<div class="componentOne">Spend</div>
+<div class="componentTwo">$ ${clicks_total} </div>
+<div class="componentThree">27.1% of budget cap $7500</div>
+<div class="componentOne">Projected Spend</div>
+<div class="componentTwo">$ 6245.55</div>
+<div class="componentThree">99.3% of budget cap $7500</div>
+</div>
+  `;
+    
+    //var firstRow = data[0];
+    //var firstCell = firstRow[queryResponse.fields.dimensions[0].name];
+
+    
+
+//element.innerHTML = clicks_total
+    
+    done()
+
+  }
+})
